16e8cc02f2941a5aa6d17f2fdf01b70f2b0b0503
danielfleshler <daniel.fle@my.hit.ac.il>
2025-09-07 18:57:08 +0300

    fix: adjust rootMargin in Navbar component for improved intersection observer behavior

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
 				},
 				{
 					root: null,
-					rootMargin: "-35% 0px -55% 0px",
+					rootMargin: "-15% 0px -70% 0px",
 					threshold: 0.2,
 				}
 			);
